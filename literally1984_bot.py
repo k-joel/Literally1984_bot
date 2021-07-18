eed9bab911b6101c7a0a5f9f44a43a5693189612
k-joel <kjoel86@gmail.com>
2021-07-19 04:31:31 +0530

    more quotes

--- literally1984_bot.py
+++ literally1984_bot.py
@@ -1,5 +1,4 @@
 import praw
-import prawcore
 import time
 import logging
 import random
@@ -50,18 +49,37 @@ QUOTE_COMMAND = "!quote 1984"
 
 QUOTES = [
     "War is peace. Freedom is slavery. Ignorance is strength.",
+    "Big Brother is Watching You.",
     "If you want a picture of the future, imagine a boot stamping on a human face—for ever.",
     "But if thought corrupts language, language can also corrupt thought.",
     "Doublethink means the power of holding two contradictory beliefs in one's mind simultaneously, and accepting both of them.",
     "Until they become conscious they will never rebel, and until after they have rebelled they cannot become conscious.",
     "Power is in tearing human minds to pieces and putting them together again in new shapes of your own choosing.",
-    "Big Brother is Watching You.",
     "Reality exists in the human mind, and nowhere else.",
-    "The best books...  are those that tell you what you know already.",
     "One does not establish a dictatorship in order to safeguard a revolution; one makes the revolution in order to establish the dictatorship.",
     "We know that no one ever seizes power with the intention of relinquishing it.",
-    "Don't you see that the whole aim of Newspeak is to narrow the range of thought? In the end we shall make thoughtcrime literally impossible, because there will be no words in which to express it."
+    "Don't you see that the whole aim of Newspeak is to narrow the range of thought? In the end we shall make thoughtcrime literally impossible, because there will be no words in which to express it.",
+    "I enjoy talking to you. Your mind appeals to me. It resembles my own mind except that you happen to be insane.",
+    "The masses never revolt of their own accord, and they never revolt merely because they are oppressed. Indeed, so long as they are not permitted to have standards of comparison, they never even become aware that they are oppressed.",
+    "The essential act of war is destruction, not necessarily of human lives, but of the products of human labour. War is a way of shattering to pieces, or pouring into the stratosphere, or sinking in the depths of the sea, materials which might otherwise be used to make the masses too comfortable, and hence, in the long run, too intelligent.",
+    "Perhaps one did not want to be loved so much as to be understood.",
+    "Who controls the past controls the future. Who controls the present controls the past.",
+    "The Ministry of Peace concerns itself with war, the Ministry of Truth with lies, the Ministry of Love with torture and the Ministry of Plenty with starvation. These contradictions are not accidental, nor do they result from from ordinary hypocrisy: they are deliberate exercises in doublethink",
+    "What can you do against the lunatic who is more intelligent than yourself, who gives your arguments a fair hearing and then simply persists in his lunacy?",
+    "The choice for mankind lies between freedom and happiness and for the great bulk of mankind, happiness is better.",
+    "You will be hollow. We shall squeeze you empty, and then we shall fill you with ourselves.",
+    "The past was alterable. The past never had been altered. Oceania was at war with Eastasia. Oceania had always been at war with Eastasia.",
+    "The best books...  are those that tell you what you know already.",
 ]
+
+QUOTE_INDEX = 0
+
+
+def get_next_quote():
+    global QUOTE_INDEX
+    quote = QUOTES[QUOTE_INDEX]
+    QUOTE_INDEX = (QUOTE_INDEX + 1) % len(QUOTES)
+    return quote
 
 
 def get_random_quote():
@@ -94,8 +112,8 @@ def main():
             comments_count += 1
             logging.info('#%s: Quote requested by u/%s in r/%s.' %
                          (str(comments_count), comment.author.name, comment.subreddit.display_name))
-            quote = get_random_quote()
-            comment.reply(quote)
+            quote = get_next_quote()
+            comment.reply("\'" + quote + "\'")
         elif len(comment.body) >= len(SEARCH_TEXT) and SEARCH_TEXT in comment.body.lower():
             comments_count += 1
             logging.info('#%s: Replying to u/%s in r/%s.' %
@@ -117,4 +135,9 @@ def main_ex():
 
 
 if __name__ == "__main__":
-    main_ex()
+    # main_ex()
+    while True:
+        i = input()
+        if i == 'q':
+            break
+        print()
